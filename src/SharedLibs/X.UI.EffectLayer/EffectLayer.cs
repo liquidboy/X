0dfa99c96331dbe8ca744c942e5dbe8d8d9cbca6
Jose Fajardo <fajardo_jf@hotmail.com>
2016-02-17 06:29:26 +1100

    LiteTab : more hacking to see what i can do with rendering xaml ..

--- src/SharedLibs/X.UI.EffectLayer/EffectLayer.cs
+++ src/SharedLibs/X.UI.EffectLayer/EffectLayer.cs
@@ -4,7 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -12,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace X.UI.EffectLayer
 {
@@ -109,6 +114,21 @@ namespace X.UI.EffectLayer
             _paths.Add(path);
         }
 
+        List<IRandomAccessStream> _streams = new List<IRandomAccessStream>();
+        public void DrawStreams(IRandomAccessStream stream)
+        {
+            _streams.Add(stream);
+        }
+
+
+        List<UIElement> _uielements = new List<UIElement>();
+        public void DrawUIElements(UIElement elm)
+        {
+            _uielements.Add(elm);
+        }
+
+
+
         //x
         private void OnDraw(CanvasControl sender, CanvasDrawEventArgs args)
         {
@@ -116,11 +136,55 @@ namespace X.UI.EffectLayer
 
             if(_paths.Count>0)
                 DoPathEffect(sender, args.DrawingSession);
+            else if (_streams.Count > 0)
+                DoStreamsEffect(sender, args.DrawingSession);
+            else if (_uielements.Count > 0)
+                DoUIElementsEffect(sender, args.DrawingSession);
             else 
                 DoEffect(args.DrawingSession, sz, (float)GlowAmount, GlowColor, ((SolidColorBrush)GlowFill).Color, ExpandAmount);
             
         }
-        
+
+        private void DoUIElementsEffect(CanvasControl sender, CanvasDrawingSession ds)
+        {
+            var sz = new Size(ParentWidth, ParentHeight);
+
+            foreach (var elm in _uielements)
+            {
+
+                var bitmap = GetUIElementBitmap(elm);
+                var pixels = bitmap.GetPixelsAsync().GetResults();
+                //var offset = (float)ExpandAmount / 2;
+                ////using (var textLayout = CreateTextLayout(ds, size))
+                //using (var cl = new CanvasCommandList(ds))
+                //{
+                //    using (var clds = cl.CreateDrawingSession())
+                //    {
+                //        stream.Seek(0);
+                //        var canvasbmp = await CanvasBitmap.LoadAsync(sender, stream);
+                //        clds.DrawImage(canvasbmp,0,0);   
+                //    }
+
+                //    glowEffectGraph.Setup(cl, (float)GlowAmount);
+                //    ds.DrawImage(glowEffectGraph.Output, offset, offset);
+                //}
+                try
+                {
+                    using (var canvasbmp = CanvasBitmap.CreateFromBytes(sender.Device, pixels.ToArray(), bitmap.PixelHeight, bitmap.PixelHeight, Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized))
+                    {
+                        ds.DrawImage(canvasbmp, 0, 0);
+                    }
+                        
+                    
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+
+            }
+        }
 
         private void DoEffect(CanvasDrawingSession ds, Size size, float amount, Windows.UI.Color glowColor, Windows.UI.Color fillColor, double expandAmount)
         {
@@ -139,6 +203,40 @@ namespace X.UI.EffectLayer
             }
         }
 
+        private async void DoStreamsEffect(CanvasControl sender, CanvasDrawingSession ds)
+        {
+            var sz = new Size(ParentWidth, ParentHeight);
+
+            foreach (var stream in _streams)
+            {
+                var offset = (float)ExpandAmount / 2;
+                //using (var textLayout = CreateTextLayout(ds, size))
+                using (var cl = new CanvasCommandList(ds))
+                {
+                    using (var clds = cl.CreateDrawingSession())
+                    {
+                        stream.Seek(0);
+                        var canvasbmp = await CanvasBitmap.LoadAsync(sender, stream);
+                        clds.DrawImage(canvasbmp, 0, 0);
+                    }
+
+                    glowEffectGraph.Setup(cl, (float)GlowAmount);
+                    ds.DrawImage(glowEffectGraph.Output, offset, offset);
+                }
+                //try {
+                //    if (stream.CanRead)
+                //    {
+                //        stream.Seek(0);
+                //        var canvasbmp = await CanvasBitmap.LoadAsync(sender, stream);
+                //        ds.DrawImage(canvasbmp, 0, 0);
+                //    }
+                //}
+                //catch (Exception ex) {
+
+                //}
+                
+            }
+        }
 
         private void DoPathEffect(CanvasControl sender, CanvasDrawingSession ds ) {
 
@@ -230,9 +328,41 @@ namespace X.UI.EffectLayer
             }
         }
 
-        
 
 
+        //public async Task<IRandomAccessStream> RenderToRandomAccessStream(UIElement element)
+        //{
+        //    RenderTargetBitmap rtb = new RenderTargetBitmap();
+        //    await rtb.RenderAsync(element);
 
+        //    var pixelBuffer = await rtb.GetPixelsAsync();
+        //    var pixels = pixelBuffer.ToArray();
+
+        //    // Useful for rendering in the correct DPI
+        //    var displayInformation = DisplayInformation.GetForCurrentView();
+
+        //    var stream = new InMemoryRandomAccessStream();
+        //    var encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.PngEncoderId, stream);
+        //    encoder.SetPixelData(BitmapPixelFormat.Bgra8,
+        //                         BitmapAlphaMode.Premultiplied,
+        //                         (uint)rtb.PixelWidth,
+        //                         (uint)rtb.PixelHeight,
+        //                         displayInformation.RawDpiX,
+        //                         displayInformation.RawDpiY,
+        //                         pixels);
+
+        //    await encoder.FlushAsync();
+        //    stream.Seek(0);
+
+        //    return stream;
+        //}
+
+
+        public RenderTargetBitmap GetUIElementBitmap(UIElement element)
+        {
+            RenderTargetBitmap bitmap = new RenderTargetBitmap();
+            bitmap.RenderAsync(element).GetResults();            
+            return bitmap;
+        }
     }
 }
--- src/SharedLibs/X.UI.LiteTab/TabList.cs
+++ src/SharedLibs/X.UI.LiteTab/TabList.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -9,7 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace X.UI.LiteTab
 {
@@ -105,10 +110,51 @@ namespace X.UI.LiteTab
             }
         }
 
-        private void TabList_Loaded(object sender, RoutedEventArgs e)
+        private async void TabList_Loaded(object sender, RoutedEventArgs e)
         {
-            if (_bkgLayer != null) _bkgLayer.InitLayer(_icTabList.ActualWidth, _icTabList.ActualHeight);
+            if (_bkgLayer != null)
+            {
+                //_bkgLayer.DrawPath("M19.9036423248472,31.7099608139106L19.9036423248472,36.3499754623481 52.4696690124692,36.3499754623481 52.4696690124692,31.7099608139106z M13.8494345230283,31.5500487045356C12.4793964904966,31.5500487045356 11.3713909668149,32.6600340560981 11.3713909668149,34.0300291732856 11.3713909668149,35.4000242904731 12.4793964904966,36.5100096420356 13.8494345230283,36.5100096420356 15.2155052704038,36.5100096420356 16.3275410217466,35.4000242904731 16.3275410217466,34.0300291732856 16.3275410217466,32.6600340560981 15.2155052704038,31.5500487045356 13.8494345230283,31.5500487045356z M19.9036423248472,20.5700682357856L19.9036423248472,25.2000731185981 52.4696690124692,25.2000731185981 52.4696690124692,20.5700682357856z M13.8494345230283,20.4100340560981C12.4793964904966,20.4100340560981 11.3713909668149,21.5100096420356 11.3713909668149,22.8800047592231 11.3713909668149,24.2499998764106 12.4793964904966,25.3599852279731 13.8494345230283,25.3599852279731 15.2155052704038,25.3599852279731 16.3275410217466,24.2499998764106 16.3275410217466,22.8800047592231 16.3275410217466,21.5100096420356 15.2155052704038,20.4100340560981 13.8494345230283,20.4100340560981z M0,0L24.0007549804868,0 32.0010067505063,7.99999987641058 63.9999996704282,7.99999987641058 63.9999996704282,18.0200194076606 63.9999996704282,28.1199949935981 63.9999996704282,47.9999998764106 29.6959415954771,47.9999998764106 21.9386784118833,47.9999998764106 0,47.9999998764106z");
+
+                _bkgLayer.DrawUIElements(_icTabList);
+                _bkgLayer.InitLayer(_icTabList.ActualWidth, _icTabList.ActualHeight);
+
+                
+            }
         }
+
+
+        //private async Task<bool> RenderUIElement() {
+
+        //    using (var stream = await RenderToRandomAccessStream())
+        //    {
+        //        var device = new CanvasDevice();
+        //        var bitmap = await CanvasBitmap.LoadAsync(device, stream);
+
+        //        var renderer = new CanvasRenderTarget(device,
+        //                                              bitmap.SizeInPixels.Width,
+        //                                              bitmap.SizeInPixels.Height, bitmap.Dpi);
+
+        //        using (var ds = renderer.CreateDrawingSession())
+        //        {
+        //            var blur = new GaussianBlurEffect();
+        //            blur.BlurAmount = 5.0f;
+        //            blur.Source = bitmap;
+        //            ds.DrawImage(blur);
+        //        }
+
+        //        stream.Seek(0);
+        //        await renderer.SaveAsync(stream, CanvasBitmapFileFormat.Png);
+
+        //        BitmapImage image = new BitmapImage();
+        //        image.SetSource(stream);
+        //        paneBackground.ImageSource = image;
+        //    }
+
+        //    return true;
+        //}
+
+
 
         protected override void OnApplyTemplate()
         {
