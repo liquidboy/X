3017014e189530c14b2e8c5590d7b0d507c11733
Jose Fajardo <fajardo_jf@hotmail.com>
2016-02-28 07:46:25 +1100

    CoreLib : brought across the sprites stuff

---
+++ src/SharedLibs/X.CoreLib/Sprites/IVisualTree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace CoreLib.Sprites
+{
+    public interface IVisualTree
+    {
+        ICollection<IVisualTreeElement> Elements { get; }
+        bool IsVisible { get; set; }
+
+
+        void Add(IVisualTreeElement element);
+
+        void Draw();
+
+        void GarbageCollect();
+    }
+}
