743308c743e4a095be6d588509429ffee681047a
Jose Fajardo <fajardo_jf@hotmail.com>
2016-02-27 15:16:32 +1100

    changed corelib to x.corelib

--- src/SharedLibs/X.CoreLib/Properties/AssemblyInfo.cs
+++ src/SharedLibs/X.CoreLib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CoreLib")]
+[assembly: AssemblyTitle("X.CoreLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoreLib")]
+[assembly: AssemblyProduct("X.CoreLib")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
