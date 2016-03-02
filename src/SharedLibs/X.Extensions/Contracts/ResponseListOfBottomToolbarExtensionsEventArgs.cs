cb6f4232d0e15e326dea6376be43bd5e14544261
Jose Fajardo <fajardo_jf@hotmail.com>
2016-03-02 22:11:33 +1100

    Extensions : started porting across the extensions bit of the sample browser app

---
+++ src/SharedLibs/X.Extensions/Contracts/ResponseListOfBottomToolbarExtensionsEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using CoreLib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X.Extensions
+{
+    public class ResponseListOfBottomToolbarExtensionsEventArgs : BaseEventArgs
+    {
+        public List<dynamic> ExtensionsMetadata;
+    }
+}
---
+++ src/SharedLibs/X.Extensions/OSShellExtensions/OSToast.cs
@@ -0,0 +1,68 @@
+﻿using CoreLib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeakEvent;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace X.Extensions.Popups
+{
+    public class OSToast : IExtension
+    {
+        private readonly WeakEventSource<EventArgs> _SendMessageSource = new WeakEventSource<EventArgs>();
+        public event EventHandler<EventArgs> SendMessage
+        {
+            add { _SendMessageSource.Subscribe(value); }
+            remove { _SendMessageSource.Unsubscribe(value); }
+        }
+
+        public IExtensionManifest ExtensionManifest { get; set; }
+        public ExtensionType ExtensionType { get; set; } = ExtensionType.OSShell;
+
+        public string Path { get; set; }
+
+        public OSToast() {
+            ExtensionManifest = new ExtensionManifest("OS Toasts", string.Empty, "Sample Extensions", "1.0", "Bubble toasts to the OS which appear in the Action Center and as a floating Toast", ExtensionInToolbarPositions.None, ExtensionInToolbarPositions.None);
+
+        }
+        
+        private bool _isExtEnabled = true;
+        public bool IsExtEnabled { get { return _isExtEnabled; } set { _isExtEnabled = value; } }
+
+        private bool _canUninstall = true;
+        public bool CanUninstall { get { return _canUninstall; } set { _canUninstall = value; } }
+        
+        public void RecieveMessage(object message)
+        {
+            if (message is OSToastEventArgs)
+            {
+                var toastTemplate = ToastTemplateType.ToastText01;
+                var toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+                var toastTextElements = toastXml.GetElementsByTagName("text");
+                toastTextElements.Item(0).InnerText = ((OSToastEventArgs)message).Text;
+
+                var toastNode = toastXml.SelectSingleNode("/toast");
+                ((XmlElement)toastNode).SetAttribute("duration", "long");
+                ((XmlElement)toastNode).SetAttribute("launch", "{\"type\":\"toast\",\"param1\":\"12345\",\"param2\":\"64890\"}");
+
+                var toast = new ToastNotification(toastXml);
+
+                ToastNotificationManager.CreateToastNotifier().Show(toast);
+            }
+        }
+
+        public void OnPaneLoad()
+        {
+
+        }
+
+        public void OnPaneUnload()
+        {
+
+        }
+    }
+}
