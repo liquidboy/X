01f37802bbc81534d6476eda34f141e4b0f290f8
Jose Fajardo <fajardo_jf@hotmail.com>
2016-04-02 14:51:16 +1100

    Viewer : added a flickr viewer in preparation for building this out with new //build/ features

--- src/SharedLibs/X.Browser.ViewModels/BrowserVM.Tabs.cs
+++ src/SharedLibs/X.Browser.ViewModels/BrowserVM.Tabs.cs
@@ -183,7 +183,8 @@ namespace X.Browser.ViewModels
             Tabs.Add(CreateDefaultTab("Apple", "http://www.apple.com/favicon.ico?v2", "http://www.apple.com", DateTime.MinValue));
             Tabs.Add(CreateDefaultTab("Google", "http://www.google.com/favicon.ico?v2", "http://www.google.com", DateTime.MinValue));
             Tabs.Add(CreateDefaultTab("YouTube", "http://www.youtube.com/favicon.ico?v2", "http://www.youtube.com", DateTime.MinValue));
-            Tabs.Add(CreateDefaultTab("Sketch", "https://www.sketchapp.com/favicon.ico?v2", "app://demo-application.sketch", DateTime.MinValue));
+            Tabs.Add(CreateDefaultTab("Sketch", "https://www.sketchapp.com/favicon.ico?v2", "viewer://sketchflow-application.sketch", DateTime.MinValue));
+            Tabs.Add(CreateDefaultTab("Flickr", "https://www.flickr.com/favicon.ico?v2", "viewer://flickr-application.flickr", DateTime.MinValue));
 
         }
 
