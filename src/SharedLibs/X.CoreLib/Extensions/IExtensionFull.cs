0435943db1a54ed1f3abcfdd217410ea722e415d
Jose Fajardo <fajardo_jf@hotmail.com>
2016-05-12 06:08:34 +1000

    Extensions : started bringing across UWP's application models extensions

---
+++ src/SharedLibs/X.CoreLib/Extensions/IExtensionFull.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppExtensions;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace CoreLib.Extensions
+{
+    public interface IExtensionFull
+    {
+        BitmapImage Logo { get; }
+        string UniqueId { get; }
+        string Version { get; }
+        bool Offline { get; }
+        AppExtension AppExtension { get; }
+        bool Enabled { get; set; }
+        bool Loaded { get; set; }
+        Task Update(AppExtension ext);
+        Task Load();
+        Task Enable();
+        void Unload();
+        void Disable();
+    }
+}
