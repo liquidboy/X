d1345c482b6c87d641059469f52ea88a9eb5b11d
Jose Fajardo <fajardo_jf@hotmail.com>
2016-05-11 16:23:13 +1000

    Settings : added api settings

---
+++ src/SharedLibs/X.UI.APIKeys/Settings.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using X.Services.Data;
+
+namespace X.UI.APIKeys
+{
+    public sealed partial class Settings : UserControl
+    {
+        public int ApiKeyCount { get; set; }
+
+        public Settings()
+        {
+            this.InitializeComponent();
+
+            RefreshData();
+        }
+
+        private void RefreshData() {
+            ApiKeyCount = StorageService.Instance.Storage.RetrieveList<APIKeyDataModel>().Count();
+        }
+
+        private void butClearKeys_Click(object sender, RoutedEventArgs e)
+        {
+            StorageService.Instance.Storage.Truncate<APIKeyDataModel>();
+            RefreshData();
+        }
+    }
+}
