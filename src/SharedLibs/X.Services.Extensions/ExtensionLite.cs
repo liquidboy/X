fe8102f87d247c36295cbef6eb5d80e749e2dd68
Jose Fajardo <fajardo_jf@hotmail.com>
2016-05-12 11:31:51 +1000

    extensions : refactor extension service

---
+++ src/SharedLibs/X.Services.Extensions/ExtensionLite.cs
@@ -0,0 +1,29 @@
+﻿using CoreLib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.Extensions.Popups;
+using System.Reflection;
+using WeakEvent;
+using System.Collections.ObjectModel;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppExtensions;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Media.Imaging;
+using X.Services.Settings;
+using Windows.Storage;
+using Windows.UI.Core;
+
+namespace X.Services.Extensions
+{
+    public struct ExtensionLite
+    {
+        public IExtensionManifest Manifest;
+        public ExtensionType ExtensionType;
+        public IExtension Extension;
+        public bool IsShowingExtensionPanel;
+    }
+    
+}
---
+++ src/SharedLibs/X.Services.Extensions/ExtensionManifest.cs
@@ -0,0 +1,71 @@
+﻿using CoreLib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.Extensions.Popups;
+using System.Reflection;
+using WeakEvent;
+using System.Collections.ObjectModel;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppExtensions;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml.Media.Imaging;
+using X.Services.Settings;
+using Windows.Storage;
+using Windows.UI.Core;
+
+namespace X.Services.Extensions
+{
+    public struct ExtensionManifest : IExtensionManifest
+    {
+        public string Abstract { get; set; }
+
+        public string AssemblyName { get; set; }
+
+        public bool CanUninstall { get; set; }
+
+        public string ContentControl { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public ExtensionInToolbarPositions FoundInToolbarPositions { get; set; }
+
+        public string IconUrl { get; set; }
+
+        public bool IsExtEnabled { get; set; }
+
+        public ExtensionInToolbarPositions LaunchInDockPositions { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Title { get; set; }
+
+        public string Path { get; set; }
+
+        public Guid UniqueID { get; set; }
+
+        public string Version { get; set; }
+
+        public ExtensionManifest(IExtensionManifest data)
+        {
+            this.Abstract = data.Abstract;
+            this.AssemblyName = data.AssemblyName;
+            this.CanUninstall = data.CanUninstall;
+            this.ContentControl = data.ContentControl;
+            this.DisplayName = data.DisplayName;
+            this.FoundInToolbarPositions = data.FoundInToolbarPositions;
+            this.IconUrl = data.IconUrl;
+            this.IsExtEnabled = data.IsExtEnabled;
+            this.LaunchInDockPositions = data.LaunchInDockPositions;
+            this.Publisher = data.Publisher;
+            this.Title = data.Title;
+            this.UniqueID = data.UniqueID;
+            this.Version = data.Version;
+            this.Path = data.Path;
+        }
+
+    }
+    
+}
