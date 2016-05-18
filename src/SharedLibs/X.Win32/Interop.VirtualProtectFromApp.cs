fc897f150bb831124d7d68443b16d33f9706b3d3
Jose Fajardo <fajardo_jf@hotmail.com>
2016-05-18 20:42:17 +1000

    Extensions : updated icon for gitx

---
+++ src/SharedLibs/X.Win32/Interop.VirtualProtectFromApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace X.Win32
+{
+    internal partial class Interop
+    {
+        [DllImport(NativeLib.Memory_L1_3, CharSet = CharSet.Unicode, SetLastError = true)]
+        internal extern static IntPtr VirtualProtectFromApp(
+            SafeHandle Address,
+            UIntPtr Size,
+            int NewProtection,
+            out IntPtr OldProtection);
+
+    }
+}
+
+
+//https://msdn.microsoft.com/en-us/library/windows/desktop/mt169846(v=vs.85).aspx
+//http://stackoverflow.com/questions/7473202/dynamic-code-execution-on-winrt-in-windows-8-either-c-or-net-c/33729808#33729808
--- src/SharedLibs/X.Win32/NativeLib.cs
+++ src/SharedLibs/X.Win32/NativeLib.cs
@@ -10,13 +10,14 @@ namespace X.Win32
 {
     public static class NativeLib
     {
-
+        internal const string Memory_L1_2 = "api-ms-win-core-memory-l1-1-2.dll";
+        internal const string Memory_L1_3 = "api-ms-win-core-memory-l1-1-3.dll";
         //[StructLayout(LayoutKind.Sequential)]
         //public struct Win32Point
         //{
         //    public int x { get; private set; }
         //    public int y { get; private set; }
-            
+
         //    internal Win32Point(int x, int y)
         //    {
         //        this.x = x;
