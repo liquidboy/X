6c4534fda4e6e4f35ec096d1d2eb02564e59635f
Jose Fajardo <fajardo_jf@hotmail.com>
2016-05-18 21:39:19 +1000

    Extensions : added services

---
+++ src/Extensions/X.Extension.ThirdParty.GixX.Services/CallService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+
+namespace X.Extension.ThirdParty.GitX.Services
+{
+    public sealed class CallService : IBackgroundTask
+    {
+        private BackgroundTaskDeferral backgroundTaskDeferral;
+        private AppServiceConnection appServiceConnection;
+
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            backgroundTaskDeferral = taskInstance.GetDeferral();
+
+            var details = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+            appServiceConnection = details.AppServiceConnection;
+            appServiceConnection.RequestReceived += AppServiceConnection_RequestReceived;
+            appServiceConnection.ServiceClosed += AppServiceConnection_ServiceClosed;
+        }
+
+        private void AppServiceConnection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            backgroundTaskDeferral?.Complete();
+        }
+
+        private async void AppServiceConnection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            var msgDef = args.GetDeferral();
+            var msg = args.Request.Message;
+            var returnData = new ValueSet();
+
+            var command = msg["Command"] as string;
+
+            switch (command) {
+                case "UI":
+                    returnData.Add("sketch-test", "X.Extension.ThirdParty.GitX.UI.Test");
+                    returnData.Add("sketch-home", "X.Extension.ThirdParty.GitX.UI.Home");
+                    break;
+            }
+            
+            await args.Request.SendResponseAsync(returnData);
+            msgDef.Complete();
+        }
+    }
+}
