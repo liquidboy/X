1b1b31bc69d7913416d4ef1a499febf1b302c303
Jose Fajardo <fajardo_jf@hotmail.com>
2016-06-04 21:55:23 +1000

    X.UI.Chrome : started work on a common shell

---
+++ src/SharedLibs/X.UI.Chrome/Shell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace X.UI.Chrome
+{
+    public sealed class Shell : Control
+    {
+        public Shell()
+        {
+            this.DefaultStyleKey = typeof(Shell);
+        }
+    }
+}
