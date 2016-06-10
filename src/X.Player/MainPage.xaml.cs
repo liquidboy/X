335ea8685f3199e5384adb2f878ba0bf9645d9a1
Jose Fajardo <fajardo_jf@hotmail.com>
2016-06-10 20:09:35 +1000

    X.Player : loading extensions

--- src/X.Player/MainPage.xaml.cs
+++ src/X.Player/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using WeakEvent;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -14,11 +15,26 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using X.Extensions;
 
 namespace X.Player
 {
-    public sealed partial class MainPage : Page, IExtensionHost
+    public sealed partial class MainPage : Page, IExtensionHost, IExtension
     {
+        public IExtensionManifest ExtensionManifest { get; set; }
+        public ExtensionType ExtensionType { get; set; } = ExtensionType.UIComponent;
+        private readonly WeakEventSource<EventArgs> _SendMessageSource = new WeakEventSource<EventArgs>();
+        public string Path { get; set; }
+
+
+
+        private bool _isExtEnabled = true;
+        public bool IsExtEnabled { get { return _isExtEnabled; } set { _isExtEnabled = value; } }
+
+        private bool _canUninstall = false;
+        public bool CanUninstall { get { return _canUninstall; } set { _canUninstall = value; } }
+
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -31,6 +47,9 @@ namespace X.Player
         
         public async Task InitExtensions()
         {
+            ExtensionManifest = new ExtensionManifest("X.Player", string.Empty, "X.Player", "1.0", "Generic X Extension Player", ExtensionInToolbarPositions.None, ExtensionInToolbarPositions.None);
+            await X.Services.Extensions.ExtensionsService.Instance.Install(this);
+
             X.Extensions.UI.Shared.ExtensionUtils.LoadThirdPartyExtensions(new List<ExtensionManifest>{
                 X.Extensions.FirstParty.Settings.Installer.GetManifest(),
             });
@@ -38,11 +57,13 @@ namespace X.Player
             await X.Services.Extensions.ExtensionsService.Instance.PopulateAllUWPExtensions();
             X.Extensions.UI.Shared.ExtensionUtils.UpdateUWPExtensionsWithStateSavedData(X.Services.Extensions.ExtensionsService.Instance.GetUWPExtensions());
 
+            ctlExtensionsBarTop.InstallMyself(); 
         }
         
         public void UnInitExtensions()
         {
             X.Services.Extensions.ExtensionsService.Instance.UnloadExtensions();
+            ctlExtensionsBarTop.UnloadExtensions();
         }
 
         public void CloseExtension(Guid extGuid)
@@ -54,5 +75,30 @@ namespace X.Player
         {
             throw new NotImplementedException();
         }
+
+        public void RecieveMessage(object message)
+        {
+            if (message is RequestListOfInstalledExtensionsEventArgs)
+            {
+                var extensions = X.Services.Extensions.ExtensionsService.Instance.GetExtensionsMetadata();
+                _SendMessageSource?.Raise(this, new ResponseListOfInstalledExtensionsEventArgs() { ExtensionsMetadata = extensions, ReceiverType = ExtensionType.UIComponent });
+            }
+            else if (message is RequestListOfTopToolbarExtensionsEventArgs)
+            {
+                var extensions = X.Services.Extensions.ExtensionsService.Instance.GetToolbarExtensionsMetadata(ExtensionInToolbarPositions.Top);
+                _SendMessageSource?.Raise(this, new ResponseListOfTopToolbarExtensionsEventArgs() { ExtensionsMetadata = extensions, ReceiverType = ExtensionType.UIComponent });
+            }
+        }
+
+        public event EventHandler<EventArgs> SendMessage
+        {
+            add { _SendMessageSource.Subscribe(value); }
+            remove { _SendMessageSource.Unsubscribe(value); }
+        }
+
+
+        public void OnPaneLoad() { }
+
+        public void OnPaneUnload() { }
     }
 }
