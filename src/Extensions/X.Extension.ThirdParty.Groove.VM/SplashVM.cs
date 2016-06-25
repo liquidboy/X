70d8e5191bfba2ee57dc58dce5aad4cc296f28c5
Jose Fajardo <fajardo_jf@hotmail.com>
2016-06-25 21:35:26 +1000

    Extensions Groove : started work on grove example

---
+++ src/Extensions/X.Extension.ThirdParty.Groove.VM/SplashVM.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace X.Extension.ThirdParty.Groove.VM
+{
+    public class SplashVM : ViewModelBase
+    {
+        private string _appId = "";
+        private string _appIdSecret = "";
+        private string _accessToken = "";
+
+        private RelayCommand<string> _requestLogin;
+
+        public RelayCommand<string> RequestLogin { get { return _requestLogin ?? (_requestLogin = new RelayCommand<string>((arg) => { AttemptLogin(); })); } }
+
+        
+
+        private void AttemptLogin() {
+
+        }
+
+        private async void MakeCall() {
+          
+        }
+
+
+    }
+}
