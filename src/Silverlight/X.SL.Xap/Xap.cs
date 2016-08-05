bb3702fa8505d02163ab472eafc5a374bc5ee42e
Jose <fajardo_jf@hotmail.com>
2016-08-05 16:11:52 +1000

    SL : xap zipping

--- src/Silverlight/X.SL.Xap/Xap.cs
+++ src/Silverlight/X.SL.Xap/Xap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace X.SL
 {
     public class Xap : IDisposable
     {
-        public string xap_dir;
-        public XamlLoader loader;
-        public DependencyObject root;
+        
+        private XamlLoader loader;
+        private DependencyObject root;
+        private string xap_dir;
+        private ZipArchive zipFile;
 
 
         public Xap(XamlLoader loader, string xap_dir, DependencyObject root) {
@@ -21,6 +24,10 @@ namespace X.SL
         public DependencyObject getRoot() { return root; }
 
         public Xap createFromFile(XamlLoader loader, string filename) {
+            throw new NotImplementedException();
+        }
+
+        public void Unpack(string fname) {
             throw new NotImplementedException();
         }
 
