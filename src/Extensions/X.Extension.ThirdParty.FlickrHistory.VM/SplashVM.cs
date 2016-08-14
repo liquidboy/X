bc1bac1af9cc0394d6da9fc2ea69d2966b92b7e6
Jose <fajardo_jf@hotmail.com>
2016-08-14 10:09:36 +1000

    X.Extensions.FlickrHistory : work on the flickr history extension that shows what the user has done in flickr over time

---
+++ src/Extensions/X.Extension.ThirdParty.FlickrHistory.VM/SplashVM.cs
@@ -0,0 +1,55 @@
+﻿using FlickrNet;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using X.CoreLib.GenericMessages;
+using X.Services.Data;
+
+
+namespace X.Extension.ThirdParty.FlickrHistory.VM
+{
+    public class SplashVM :ViewModelBase
+    {
+        public string Title { get; set; } = "Splash";
+        public string Version { get; set; } = "1.0.0.1";
+        public string GroupingType { get; set; } = "FlickrHistory";
+        public string HostPackageFamilyName { get; set; } = "e10f99d4-4b05-4716-8c6a-4588ec9bef21_1v77q6cebkz10";
+
+
+        private object _comments;
+        public object Comments { get { return _comments; } set { _comments = value; RaisePropertyChanged(); } }
+
+
+        public SplashVM() {
+            LoadMessangerRegistrations();
+
+            //Messenger.Default.Send(new RequestPhotoComments());
+        }
+
+        private void LoadMessangerRegistrations()
+        {
+            //Messenger.Default.Register<LoadPhotoComments>(this, DoLoadPhotoComments);
+        }
+
+        //private void DoLoadPhotoComments(LoadPhotoComments msg) {
+        //    Comments = msg.Comments;
+        //}
+
+        public void UnloadMessangerRegistrations()
+        {
+            //Messenger.Default.Unregister<LoadPhotoComments>(this, DoLoadPhotoComments);
+        }
+    }
+}
