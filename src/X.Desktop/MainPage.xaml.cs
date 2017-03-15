347986b3c800018d98b1c4330889e2a2045c5a43
jose.fajardo <fajardo_jf@hotmail.com>
2017-03-15 22:04:29 +1100

    more examples

--- src/X.Desktop/MainPage.xaml.cs
+++ src/X.Desktop/MainPage.xaml.cs
@@ -28,6 +28,23 @@ namespace X.Desktop
             public double TotalCost { get; set; }
         }
 
+        public class OrderLiteItem : SQLiteDataEntity 
+        {
+            public Guid OrderID { get; set; }
+            public long Quantity { get; set; }
+            public string Product { get; set; }
+            public double UnitCost  { get; set; }
+            public int OrderHeaderId { get; set; }
+        }
+
+        public class OrderFooter : SQLiteDataEntity
+        {
+            public Guid OrderID { get; set; }
+            public string ShippingAddress { get; set; }
+            public string PickupBy { get; set; }
+            public int OrderHeaderId { get; set; }
+        }
+
 
         public MainPage()
         {
@@ -41,18 +58,26 @@ namespace X.Desktop
             this.InitializeComponent();
             
             AppDatabase.Current.Init();
-            
+
             //create new
-            var oh = new OrderHeader();
-            oh.OrderID = Guid.NewGuid();
-            oh.Title = "test title";
-            oh.DateStamp = DateTime.UtcNow;
-            oh.Quantity = 100;
-            oh.TotalCost = 199.00;
+            var oh = new OrderHeader() { OrderID = Guid.NewGuid(), Title = "test title",
+                DateStamp = DateTime.UtcNow, Quantity = 100, TotalCost = 199.00 };
             var newid = oh.Save();
 
+            var oli = new OrderLiteItem() { Quantity = 1, Product = "XBox Scorpio", UnitCost = 499.00,
+                OrderID = oh.OrderID, OrderHeaderId = newid };
+            oli.Save();
+
+            var of = new OrderFooter() { OrderID = oh.OrderID, PickupBy = "jose",
+                ShippingAddress = "Home", OrderHeaderId = newid };
+            of.Save();
+
+
             //search
-            var result = oh.Find("id = " + newid);
+            var resultOrderHeader = oh.Find("id = " + newid);
+            var resultOrderLineItems = oli.Find("'OrderHeaderId' = " + newid );
+            var resultOrderFooter = of.Find("'ShippingAddress' = 'Home'" );
+
 
             //load 
             if (oh.Retrieve(newid))
