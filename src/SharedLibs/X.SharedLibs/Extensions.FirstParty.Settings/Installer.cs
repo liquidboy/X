54170d2d41417ef3d84c80bc89d05bc899a0d3d8
jose.fajardo <fajardo_jf@hotmail.com>
2017-04-23 13:53:42 +1000

    fixing extensions list

--- src/SharedLibs/X.SharedLibs/Extensions.FirstParty.Settings/Installer.cs
+++ src/SharedLibs/X.SharedLibs/Extensions.FirstParty.Settings/Installer.cs
@@ -11,7 +11,7 @@ namespace X.Extensions.FirstParty.Settings
     {
         public static ExtensionManifest GetManifest() {
 
-            return new ExtensionManifest("Settings Management", "ms-appx:///Extensions/FirstParty/Settings/Settings.png", "Jose Fajardo", "1.0", "Manage your Settings", ExtensionInToolbarPositions.Top, ExtensionInToolbarPositions.Right) { ContentControl = "X.Extensions.FirstParty.Settings.Content", AssemblyName= "X.Extensions.FirstParty.Settings", IsExtEnabled = false };
+            return new ExtensionManifest("Settings Management", "ms-appx:///Extensions/FirstParty/Settings/Settings.png", "Jose Fajardo", "1.0", "Manage your Settings", ExtensionInToolbarPositions.Top, ExtensionInToolbarPositions.Right) { ContentControl = "X.Extensions.FirstParty.Settings.Content", AssemblyName= "X.SharedLibs", IsExtEnabled = false };
         }
     }
 }
