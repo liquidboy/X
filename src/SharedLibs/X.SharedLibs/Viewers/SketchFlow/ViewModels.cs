249c163e7e9f1e0e7f67412978edb97e7dae704d
jose.fajardo <fajardo_jf@hotmail.com>
2018-06-10 14:57:25 +1000

    sketchflow : cleaned up page layer logic

--- src/SharedLibs/X.SharedLibs/Viewers/SketchFlow/ViewModels.cs
+++ src/SharedLibs/X.SharedLibs/Viewers/SketchFlow/ViewModels.cs
@@ -9,95 +9,104 @@ using X.Services.Data;
 
 namespace X.Viewer.SketchFlow
 {
-    public class Sketch :ViewModelBase
+  public class Sketch : ViewModelBase
+  {
+    public ObservableCollection<SketchPage> Pages { get; set; }
+
+    private SketchPage _SelectedPage;
+    public SketchPage SelectedPage
     {
-        public ObservableCollection<SketchPage> Pages { get; set; }
-
-        private SketchPage _SelectedPage;
-        public SketchPage SelectedPage {
-            get { return _SelectedPage; }
-            set {
-                if(_SelectedPage != null)_SelectedPage.IsSelected = false;
-                _SelectedPage = value;
-                RaisePropertyChanged();
-                _SelectedPage.IsSelected = true;
-            }
-        }
-        
-        public Sketch() {
-            Pages = new ObservableCollection<SketchPage>();
-        }
-
-        public void ExternalPC(string propertyName)
-        {
-            RaisePropertyChanged(propertyName);
-        }
+      get { return _SelectedPage; }
+      set
+      {
+        if (_SelectedPage != null) _SelectedPage.IsSelected = false;
+        _SelectedPage = value;
+        RaisePropertyChanged();
+        _SelectedPage.IsSelected = true;
+      }
     }
 
-    public class SketchPage : ViewModelBase
+    public Sketch()
     {
-        private string _title;
-        public string Title { get { return _title; } set { _title = value; RaisePropertyChanged(); } }
-        public int Left { get; set; }
-        public int Top { get; set; }
-        public int Width { get; set; }
-        public int Height { get; set; }
-        private bool _IsSelected;
-        public bool IsSelected { get { return _IsSelected; } set { _IsSelected = value; RaisePropertyChanged(); } }
-
-        public ObservableCollection<PageLayer> Layers { get; set; }
-
-        public List<PageLayer> ExpandedLayers { get { return Layers.Where(x => x.IsExpanded).ToList(); } private set { } }
-
-        public int Id { get; set; }
-        
-        public SketchPage()
-        {
-            Layers = new ObservableCollection<PageLayer>();
-        }
-        public void ExternalPC(string propertyName) {
-            RaisePropertyChanged(propertyName);
-        }
+      Pages = new ObservableCollection<SketchPage>();
     }
 
-    public class PageLayer : ViewModelBase
+    public void ExternalPC(string propertyName)
     {
-        private bool _isEnabled = true;
-        public bool IsEnabled { get { return _isEnabled; } set { _isEnabled = value; RaisePropertyChanged(); } } 
-        public bool HasChildContainerCanvas { get; set; } = false;
+      RaisePropertyChanged(propertyName);
+    }
+  }
 
-        private bool _isExpanded = false;
-        public bool IsExpanded { get { return _isExpanded; } set { _isExpanded = value; RaisePropertyChanged(); } }
+  public class SketchPage : ViewModelBase
+  {
+    private string _title;
+    public string Title { get { return _title; } set { _title = value; RaisePropertyChanged(); } }
+    public int Left { get; set; }
+    public int Top { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    private bool _IsSelected;
+    public bool IsSelected { get { return _IsSelected; } set { _IsSelected = value; RaisePropertyChanged(); } }
+
+    public ObservableCollection<PageLayer> Layers { get; set; }
+    public PageLayer SelectedLayer { get; set; }
+
+    public List<PageLayer> ExpandedLayers { get { return Layers.Where(x => x.IsExpanded).ToList(); } private set { } }
+
+    public int Id { get; set; }
+
+    public SketchPage()
+    {
+      Layers = new ObservableCollection<PageLayer>();
+      var defaultLayer = new PageLayer();
+      Layers.Add(defaultLayer);
+      //SelectedLayer = defaultLayer;
+    }
+    public void ExternalPC(string propertyName)
+    {
+      RaisePropertyChanged(propertyName);
+    }
+  }
+
+  public class PageLayer : ViewModelBase
+  {
+    private bool _isEnabled = true;
+    public bool IsEnabled { get { return _isEnabled; } set { _isEnabled = value; RaisePropertyChanged(); } }
+    public bool HasChildContainerCanvas { get; set; } = false;
+
+    private bool _isExpanded = false;
+    public bool IsExpanded { get { return _isExpanded; } set { _isExpanded = value; RaisePropertyChanged(); } }
 
 
-        private string _layerNumber;
-        public string LayerNumber { get { return _layerNumber; } set { _layerNumber = value; RaisePropertyChanged(); } }
+    private string _layerNumber;
+    public string LayerNumber { get { return _layerNumber; } set { _layerNumber = value; RaisePropertyChanged(); } }
 
-        public ObservableCollection<XamlFragment> XamlFragments { get; set; }
+    public ObservableCollection<XamlFragment> XamlFragments { get; set; }
 
-        public int PersistedId { get; set; }  //used to link this to the persistedId
+    public int PersistedId { get; set; }  //used to link this to the persistedId
 
-        public PageLayer() {
-            XamlFragments = new ObservableCollection<XamlFragment>();
-        }
-
-        public void ExternalPC(string propertyName)
-        {
-            RaisePropertyChanged(propertyName);
-        }
+    public PageLayer()
+    {
+      XamlFragments = new ObservableCollection<XamlFragment>();
     }
 
-
-    public class XamlFragment: ViewModelBase
+    public void ExternalPC(string propertyName)
     {
-        public string Xaml { get; set; }
-        public string Uid { get; set; } = new Guid().ToString();
-
-        public Type Type { get; set; }
-        public string Data { get; set; }
-
-        private bool _isEnabled = true;
-        public bool IsEnabled { get { return _isEnabled; } set { _isEnabled = value; RaisePropertyChanged(); } }
+      RaisePropertyChanged(propertyName);
     }
+  }
+
+
+  public class XamlFragment : ViewModelBase
+  {
+    public string Xaml { get; set; }
+    public string Uid { get; set; } = new Guid().ToString();
+
+    public Type Type { get; set; }
+    public string Data { get; set; }
+
+    private bool _isEnabled = true;
+    public bool IsEnabled { get { return _isEnabled; } set { _isEnabled = value; RaisePropertyChanged(); } }
+  }
 
 }
