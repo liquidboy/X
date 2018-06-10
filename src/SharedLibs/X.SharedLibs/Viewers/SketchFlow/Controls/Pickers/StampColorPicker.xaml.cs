1975530762b7ce4ed0a962256124666ff6eed9d5
jose.fajardo <fajardo_jf@hotmail.com>
2018-06-10 16:12:17 +1000

    sketchflow : using xaml colorpicker

--- src/SharedLibs/X.SharedLibs/Viewers/SketchFlow/Controls/Pickers/StampColorPicker.xaml.cs
+++ src/SharedLibs/X.SharedLibs/Viewers/SketchFlow/Controls/Pickers/StampColorPicker.xaml.cs
@@ -1,28 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
-
+using Windows.UI.Xaml.Media;
 
 namespace X.Viewer.SketchFlow.Controls.Pickers
 {
-    public sealed partial class StampColorPicker : UserControl
+  public sealed partial class StampColorPicker : UserControl
+  {
+    public event EventHandler ColorChanged;
+
+    public List<String> ColorTypes { get; set; } = new List<string>();
+
+    public StampColorPicker()
     {
-        public event EventHandler ColorChanged;
-
-        public List<String> ColorTypes { get; set; } = new List<string>();
-
-        public StampColorPicker()
-        {
-            this.InitializeComponent();
-        }
-
-        private void cs_ColorChanged(object sender, EventArgs e)
-        {
-            ColorChanged?.Invoke(sender, new ColorPickerEventArgs() { ColorType = (string)rcb.Value2  } );
-        }
+      this.InitializeComponent();
     }
 
-    public class ColorPickerEventArgs : EventArgs {
-        public string ColorType;
+    private void cs_ColorChanged(object sender, EventArgs e)
+    {
+      ColorChanged?.Invoke(sender, new ColorPickerEventArgs() { ColorType = (string)rcb.Value2 });
     }
+
+    private void cs2_ColorChanged(ColorPicker sender, ColorChangedEventArgs args)
+    {
+      ColorChanged?.Invoke( new SolidColorBrush(Color.FromArgb(args.NewColor.A, args.NewColor.R, args.NewColor.G, args.NewColor.B)), new ColorPickerEventArgs() { ColorType = (string)rcb.Value2 });
+    }
+  }
+
+  public class ColorPickerEventArgs : EventArgs
+  {
+    public string ColorType;
+  }
 }
