8993c60227d7f74fa76360f46d195cb234713f52
jose.fajardo <fajardo_jf@hotmail.com>
2018-09-02 08:16:39 +1000

    as : sample to learn astarsearch

---
+++ src/SampleAStar/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace SampleAStar
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+
+      
+    }
+
+    private void layoutRoot_Loaded(object sender, RoutedEventArgs e)
+    {
+      DrawRoute(new Point(2, 4));
+
+    }
+
+    private void ClearRoute() {
+      foreach (VisualNode vn in cvLayout.Children) {
+        vn.NodeClicked -= Vn_NodeClicked;
+      }
+      cvLayout.Children.Clear();
+    }
+
+    private void DrawRoute(Point end) {
+      //DRAW MAP
+      for (int y = 0; y < SampleAStar.Map.MAP_HEIGHT; y++)
+      {
+        for (int x = 0; x < SampleAStar.Map.MAP_WIDTH; x++)
+        {
+          var wh = 40d;
+          var pt = new Point(x * wh, y * wh);
+
+          VisualNode vn = new VisualNode();
+          vn.Name = $"vnx{x}y{y}";
+          vn.SetValue(Canvas.LeftProperty, pt.X);
+          vn.SetValue(Canvas.TopProperty, pt.Y);
+          vn.Width = wh;
+          vn.Height = wh;
+          vn.DrawPosition(x, y);
+          vn.SetDot(false);
+          vn.SetWall(SampleAStar.Map.GetMap(x, y));
+          vn.SetPosition(x, y);
+          vn.NodeClicked += Vn_NodeClicked;
+          cvLayout.Children.Add(vn);
+        }
+      }
+
+      //START STAR SEARCH THROUGH MAP
+      AStarExample.Start(cvLayout, end);
+    }
+
+    private void Vn_NodeClicked(object sender, EventArgs e)
+    {
+      string positionAsString = (string)sender;
+      var positionAsParts = positionAsString.Split(",".ToCharArray());
+      ClearRoute();
+      DrawRoute(new Point(int.Parse(positionAsParts[0]), int.Parse(positionAsParts[1])));
+    }
+  }
+}
---
+++ src/SampleAStar/VisualNode.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace SampleAStar
+{
+  public sealed partial class VisualNode : UserControl
+  {
+    public event EventHandler NodeClicked;
+    private string _position;
+
+    public VisualNode()
+    {
+      this.InitializeComponent();
+    }
+
+    public void DrawPosition(double x, double y) {
+      labelPosition.Text = $"{x},{y}";
+    }
+
+    public void SetDot(bool isVisible) {
+      elDot.Visibility = isVisible? Visibility.Visible: Visibility.Collapsed ;
+    }
+
+    public void SetWall(int status) {
+      switch (status) {
+        case 9:
+          recWall.Opacity = 100;
+          labelPosition.Foreground = new SolidColorBrush(Colors.White);
+          break;
+        default:
+          recWall.Opacity = 0;
+          break;
+      }
+    }
+
+    public void SetPosition(int x, int y) { _position = $"{x},{y}"; }
+    private void layoutRoot_PointerReleased(object sender, PointerRoutedEventArgs e)
+    {
+      NodeClicked?.Invoke(_position, new EventArgs());
+    }
+  }
+}
