31a8a753465ca0ec8a213d6cea3ef80004e7e22b
jose.fajardo <fajardo_jf@hotmail.com>
2018-10-13 10:52:11 +1100

    Fixing up references

--- X.ModernDesktop/App.xaml.cs
+++ X.ModernDesktop/App.xaml.cs
@@ -70,7 +70,7 @@ namespace X.ModernDesktop
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(SimTowerPage), e.Arguments);
+                    rootFrame.Navigate(typeof(SQLitePage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- src/SharedLibs/X.SharedLibs/Browser.ViewModels/ViewModelLocator.cs
+++ src/SharedLibs/X.SharedLibs/Browser.ViewModels/ViewModelLocator.cs
@@ -1,5 +1,5 @@
-﻿using GalaSoft.MvvmLight.Ioc;
-using Microsoft.Practices.ServiceLocation;
+﻿using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
