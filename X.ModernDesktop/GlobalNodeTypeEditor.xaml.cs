6bc8e6d799be1052107a27a81720a8d2302452d8
Jose.Fajardo <fajardo_jf@hotmail.com>
2019-04-21 13:02:37 +1000

    NodeGraph : #40 Tool now works to update existing entity

--- X.ModernDesktop/GlobalNodeTypeEditor.xaml.cs
+++ X.ModernDesktop/GlobalNodeTypeEditor.xaml.cs
@@ -26,19 +26,13 @@ namespace X.ModernDesktop
 
     public sealed partial class GlobalNodeTypeEditor : Page
     {
-        //Assembly _foundControls;
-        //Type[] _foundControlTypes;
-        //private static List<CloudNodeTypeEntity> _nodeTypeMetadata;
 
         public ObservableCollection<CloudNodeTypeEntity> AllControls { get; set; }
         public CollectionViewSource FilteredControlsCVS { get; set; }
         public ObservableCollection<CloudNodeTypeEntity> FilterdControls { get; set; }
 
-        //public ObservableCollection<ControlMetaData> FilterdControlsEnums { get; set; }
-
         public GlobalNodeTypeEditor()
         {
-            //_nodeTypeMetadata = new List<CloudNodeTypeEntity>();
             NodeGraphGlobalStorage.Current.InitializeGlobalStorage(App.AzureConnectionString);
 
             FilteredControlsCVS = new CollectionViewSource();
@@ -48,32 +42,23 @@ namespace X.ModernDesktop
             FilterdControls = new ObservableCollection<CloudNodeTypeEntity>();
             FilteredControlsCVS.Source = FilterdControls;
 
-            //FilterdControlsEnums = new ObservableCollection<ControlMetaData>();
-
             this.InitializeComponent();
 
             Task.Run(async () => {
                 await FillFromGlobalStorage();
                 await Dispatcher.RunAsync( Windows.UI.Core.CoreDispatcherPriority.Normal, ()=> FilterData(""));  //<-- back to UI
             }); 
-            
         }
 
 
         private async Task<bool> FillFromGlobalStorage()
         {
-            //var globalData = await RetrieveGlobalNodeTypes("Dots");
             var globalData = await NodeGraphGlobalStorage.Current.RetrieveAllGlobalNodeTypes();
             if (globalData.Count > 0) { 
                 
                 foreach (CloudNodeTypeEntity item in globalData.Results)
                 {
-                    //_nodeTypeMetadata.Add(new CloudNodeTypeMetadata(item.RowKey, item.PartitionKey, item.InputNodeSlots, item.InputNodeSlotCount, item.OutputNodeSlots, item.OutputNodeSlotCount, item.Color, item.View));
-
-                    //var ncmd = new ControlMetaData() { Name = item.RowKey, FullName = $"{item.RowKey} ({item.PartitionKey})" };
-
                     AllControls.Add(item);
-                    //FilterdControlsEnums.Add(ncmd);
                 }
             }
             return true;
@@ -105,10 +90,7 @@ namespace X.ModernDesktop
 
         public async void SaveSelectedItem()
         {
-            var selectedItem = (CloudNodeTypeEntity)grdSelectedItem.DataContext;
-           
-            //entity.Color = selItem.
-            await NodeGraphGlobalStorage.Current.SaveGlobalNodeType(selectedItem);
+            await NodeGraphGlobalStorage.Current.SaveGlobalNodeType((CloudNodeTypeEntity)grdSelectedItem.DataContext);
         }
 
         public void CreateNewItem()
@@ -120,41 +102,6 @@ namespace X.ModernDesktop
         {
 
         }
-
-        //public static T Clone<T>(T source)
-        //{
-        //    //string objXaml = XamlWriter.Save(source); //Serialization
-        //    XmlSerializer serializer = new XmlSerializer(typeof(T));
-        //    StringWriter stringWriter = new StringWriter();
-        //    serializer.Serialize(stringWriter, source);
-        //    string objXaml = stringWriter.ToString();
-
-        //    //StringReader stringReader = new StringReader(objXaml);
-        //    //XmlReader xmlReader = XmlReader.Create(stringReader);
-        //    //T t = (T)XamlReader.Load(xmlReader); //Deserialization
-        //    T t = (T)XamlReader.Load(objXaml); //Deserialization
-        //    return t;
-        //}
-
-        //https://social.msdn.microsoft.com/Forums/vstudio/en-US/de2b3198-74e9-4465-a0e1-7b435014ccdd/is-there-any-way-to-serialize-the-style-?forum=wpf
-        //https://blogs.u2u.be/diederik/post/serializing-and-deserializing-data-in-a-windows-store-app
     }
-
-    //public class FilterByCollection<T> : ObservableCollection<T>
-    //{
-    //    public object Key { get; set; }
-
-    //    public new IEnumerator<T> GetEnumerator()
-    //    {
-    //        return (IEnumerator<T>)base.GetEnumerator();
-    //    }
-    //}
-
-    //public class ControlMetaData : INotifyPropertyChanged {
-    //    public string Name { get; set; }
-    //    public string FullName { get; set; }
-
-    //    public event PropertyChangedEventHandler PropertyChanged;
-    //}
 
 }
